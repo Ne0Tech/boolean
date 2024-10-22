bf8b041d0edb9a88a1dbf14fa44eb0eb32492c16
Zavian Pedigo <pedigzav001@warren.k12.in.us>
2024-10-22 10:15:29 -0400

    nuh uh

---
+++ Main.py
@@ -0,0 +1,12 @@
+
+#  input function
+#  instructing the user to enter their age.
+age = int(input("Enter your age: "))
+
+
+# YOU are are eligible if your age is greater than or equal
+# to 18.
+is_eligible = age >= 18
+
+
+print ("Is eligible to vote: ", is_eligible)
